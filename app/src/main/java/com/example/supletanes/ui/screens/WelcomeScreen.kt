d4209e4a011768e750370dc8eec8d76776ada857
Nicolas Caro <nicolascarodittus@gmail.com>
2025-10-05 22:41:36 -0300

    Arreglo imagen WelcomeScreen.kt / barra de navegación inferior en MainScreen.kt / Agregar pantallas productos, perfil y carrito

--- app/src/main/java/com/example/supletanes/ui/screens/WelcomeScreen.kt
+++ app/src/main/java/com/example/supletanes/ui/screens/WelcomeScreen.kt
@@ -1,5 +1,6 @@
 package com.example.supletanes.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -7,9 +8,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.supletanes.R
 
 @Composable
 fun WelcomeScreen(onContinueClicked: () -> Unit) {
@@ -20,8 +23,13 @@ fun WelcomeScreen(onContinueClicked: () -> Unit) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.welogo),
+            contentDescription = "Imagen de bienvenida", // Descripción para accesibilidad
+            modifier = Modifier.size(200.dp) // O cualquier otro
+        )
         Text(
-            text = "Bienvenido a Suplementos Pro",
+            text = "Bienvenido a Supletanes",
             style = MaterialTheme.typography.headlineLarge, // Título más grande
             textAlign = TextAlign.Center
         )
